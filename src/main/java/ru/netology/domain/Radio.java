5d17e55bffa16c83fdc0c44ed099b92ed757a34d
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2023-12-08 11:02:01 +0300

    rewrite Radio.java by lombok

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,14 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
 public class Radio {
 
     private int stationsQuantity = 10;
@@ -11,103 +20,5 @@ public class Radio {
     private int maxVolume = 100;
     private int currentVolume;
 
-    public Radio() {
 
-    }
-
-    public Radio(int stationsQuantity) {
-
-        this.stationsQuantity = stationsQuantity;
-        this.maxStationNumber = stationsQuantity - 1;
-    }
-
-    public int getStationsQuantity() {
-        return stationsQuantity;
-    }
-
-    public int getMinStationNumber() {
-        return minStationNumber;
-    }
-
-    public int getMaxStationNumber() {
-        return maxStationNumber;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setStationsQuantity(int stationsQuantity) {
-
-        this.stationsQuantity = stationsQuantity;
-        this.maxStationNumber = stationsQuantity - 1;
-
-    }
-
-    public void setStationNumber(int newStationNumber) {
-        if (newStationNumber < minStationNumber) {
-            return;
-        }
-        if (newStationNumber > maxStationNumber) {
-            return;
-        }
-        currentStation = newStationNumber;
-    }
-
-
-    public void setCurrentVolume(int newCurrentVolume) {
-
-        if (newCurrentVolume < minVolume) {
-            return;
-        }
-        if (newCurrentVolume > maxVolume) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
-    }
-
-    public void switchToNextStation() {
-        if (currentStation < maxStationNumber) {
-            currentStation = currentStation + 1;
-        } else {
-            currentStation = minStationNumber;
-        }
-        setStationNumber(currentStation);
-    }
-
-    public void switchToPreviousStation() {
-        if (currentStation > minStationNumber) {
-            currentStation = currentStation - 1;
-        } else {
-            currentStation = maxStationNumber;
-        }
-        setStationNumber(currentStation);
-    }
-
-
-    public void increaseVolume() {
-        if (currentVolume < maxVolume) {
-            currentVolume = currentVolume + 1;
-        }
-        setCurrentVolume(currentVolume);
-    }
-
-    public void decreaseVolume() {
-        if (currentVolume > minVolume) {
-            currentVolume = currentVolume - 1;
-        }
-        setCurrentVolume(currentVolume);
-    }
 }
